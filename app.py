14c2f22d53f0272a735b69931ab9d6ff771f3bf2
carl sednaoui <carlsednaoui@gmail.com>
2013-09-16 22:43:34 -0400

    Adds app shell

---
+++ app.py
@@ -0,0 +1,20 @@
+'''
+These are the basic functions needed to get started.
+We can worry about editing and deleting users, questions, answers and quizzes 
+at a later date.
+'''
+
+def create_user(email, password):
+  pass
+
+def create_quiz(user_id):
+  pass
+
+def create_question(quiz_id):
+  pass
+
+def create_answer(question_id):
+  pass
+
+def show_quiz(quiz_id):
+  pass
