b9062b557b3347db4199428676360954db89781d
carl sednaoui <carlsednaoui@gmail.com>
2013-09-18 21:59:37 -0400

    Starts flask API development

--- app.py
+++ app.py
@@ -4,17 +4,40 @@ We can worry about editing and deleting users, questions, answers and quizzes
 at a later date.
 '''
 
-def create_user(email, password):
-  pass
+import flask
+app = flask.Flask(__name__)
+
+DEBUG = True
+app.config.from_object(__name__)
 
 def create_quiz(user_id):
-  pass
+    pass
 
 def create_question(quiz_id):
-  pass
+    pass
 
 def create_answer(question_id):
-  pass
+    pass
 
 def show_quiz(quiz_id):
-  pass
+    pass
+
+@app.route('/create-user', methods=['POST', 'GET'])
+def create_user():
+    data = flask.request.form
+    print data['email']
+    return "Returning"
+
+'''
+Use curl to post data and capture it in the create_user fn.
+Once you get the data, do something!
+
+What tool do you need to parse the JSON from the post?
+
+Do you use request.form, request.json or request.data?
+Figure out which one to use when.
+'''
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
