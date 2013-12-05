d3d304454240fdf691325f51f155e77bc834e750
carl sednaoui <carlsednaoui@gmail.com>
2013-12-04 23:05:19 -0500

    Updates app to use SQLAchemy

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app = flask.Flask(__name__)
 DEBUG = True
 app.config.from_object(__name__)
 
+
 @app.route('/create-user', methods=['POST', 'GET'])
 def create_user():
     data = flask.request.json
@@ -22,7 +23,7 @@ def create_user():
     m.update(data['password'])
     hashed_password = m.hexdigest()
 
-    # # Save it to DB
+    # # Save it to DB via SQL
     # conn = sqlite3.connect('database.db')
     # conn.cursor().execute("INSERT INTO users VALUES (?, ?)", [data['email'], hashed_password])
     # conn.commit()
@@ -36,36 +37,35 @@ def create_user():
     # Call log_in fn
     return flask.jsonify({'email': data['email'], 'password': hashed_password})
 
+
 @app.route('/users')
 def list_users():
   users = []
 
-  conn = sqlite3.connect('database.db')
-  db_response = conn.cursor().execute("SELECT email FROM users")
+  db_response = Session().query(User.email).all()
   for row in db_response:
     users.append(row)
-  conn.close()
-  
+
   return flask.jsonify({'result': users})
+
 
 @app.route('/login', methods=['POST'])
 def log_in():
   data = flask.request.json
 
-  email = data['email']
+  user_email = data['email']
   hashed_password = md5.new(data['password']).hexdigest()
 
-  conn = sqlite3.connect('database.db')
-  db_response = conn.cursor().execute("SELECT email FROM users WHERE email = ? AND password = ?", [email, hashed_password])
+  s = Session()
+  res = s.query(User).filter_by(email=user_email, password=hashed_password).all()
 
-  if len(db_response.fetchall()) == 1:
-    conn.close()
+  if len(res) == 1:
     return flask.jsonify({'response': 'yes!'})
   else:
-    conn.close()
     return flask.jsonify({'response': 'go away!'})
 
   # Return user ID and create session cookie
+
 
 @app.route('/create-quiz', methods=['POST'])
 def create_quiz():
@@ -73,46 +73,46 @@ def create_quiz():
   Requires email, title
   """
   data = flask.request.json
-  conn = sqlite3.connect('database.db')
-  user_id = conn.cursor().execute("SELECT id FROM users WHERE email = ?", [data['email']])
-  conn.cursor().execute("INSERT INTO quizzes ('user_id', 'title') VALUES (?, ?)", [str(user_id), data['title']])
-  conn.commit()
-  conn.close()
+  
+  s = Session()
+  _user_id = s.query(User.id).filter_by(email=data['email']).all()[0]
+  quiz = Quiz(user_id = str(_user_id), title = data['title'])
+  s.add(quiz)
+  s.commit()
 
   return flask.jsonify({'email': data['email'], 'title': data['title']})
+
 
 @app.route('/quizzes')
 def get_quizzes():
   quizzes = []
 
-  conn = sqlite3.connect('database.db')
-  db_response = conn.cursor().execute("SELECT title FROM quizzes")
+  db_response = Session().query(Quiz.title).all()
   for row in db_response:
     quizzes.append(row)
-  conn.close()
 
   return flask.jsonify({'result': quizzes})
+
 
 @app.route('/create-question', methods=['POST'])
 def create_question():
   data = flask.request.json
 
-  conn = sqlite3.connect('database.db')
-  conn.cursor().execute("INSERT INTO questions ('quiz_id', 'title') VALUES (?, ?)", [data['quiz_id'], data['title']])
-  conn.commit()
-  conn.close()
+  s = Session()
+  question = Question(quiz_id = data['quiz_id'], title = data['title'])
+  s.add(question)
+  s.commit()
 
   return flask.jsonify({'quiz_id': data['quiz_id'], 'title': data['title']})
+
 
 @app.route('/questions')
 def get_questions():
   questions = []
 
-  conn = sqlite3.connect('database.db')
-  db_response = conn.cursor().execute("SELECT title FROM questions")
+  db_response = Session().query(Question.title).all()
   for row in db_response:
     questions.append(row)
-  conn.close()
 
   return flask.jsonify({'result': questions})
 
